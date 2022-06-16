0d8553bc065aae51b8830b3b53d0eb05ee8802e9
natan <natan653@gmail.com>
2022-06-16 23:38:36 +0300

    commit 6- heroku live

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g6+a*lyu8dvko@qq0)gmjx-ppk%2+egycjblr!i43&*smb+utu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://natan-to-do-list.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
